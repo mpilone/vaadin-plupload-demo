2f9ccf890e52afb2a6a6a5a6dc7484ff49566066
Mike Pilone <mpilone@npr.org>
2014-02-13 21:07:32 -0500

    Updated for v1.1.

--- src/main/java/org/mpilone/vaadin/PluploadDemo.java
+++ src/main/java/org/mpilone/vaadin/PluploadDemo.java
@@ -4,6 +4,9 @@ import static java.lang.String.format;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 
 import com.vaadin.annotations.Push;
@@ -22,19 +25,27 @@ import com.vaadin.ui.*;
 @Push(value = PushMode.DISABLED)
 public class PluploadDemo extends UI {
 
+  private final static String FULL_WIDTH = "100%";
+
   private TextArea logTxt;
 
-  private VerticalLayout contentLayout;
+  private VerticalLayout leftColumnLayout;
 
   @Override
   protected void init(VaadinRequest request) {
 
     setPollInterval(3000);
+    setWidth(FULL_WIDTH);
 
-    contentLayout = new VerticalLayout();
-    contentLayout.setSpacing(true);
+    HorizontalLayout contentLayout = new HorizontalLayout();
     contentLayout.setMargin(true);
+    contentLayout.setSpacing(true);
+    contentLayout.setWidth(FULL_WIDTH);
     setContent(contentLayout);
+
+    leftColumnLayout = new VerticalLayout();
+    leftColumnLayout.setSpacing(true);
+    contentLayout.addComponent(leftColumnLayout);
 
     // Upload 1: Manual submit button.
     Plupload upload = buildPlupload();
@@ -62,18 +73,22 @@ public class PluploadDemo extends UI {
     // Upload 4: Immediate submit using sleep.
     upload = buildPlupload();
     upload.setButtonCaption("Immediate Submit Forced Slow");
-    upload.setReceiver(new SlowReceiver(10));
+    upload.setReceiver(new DemoReceiver(new SlowOutputStream()));
     upload.setImmediate(true);
     addExample("Slow it Down", upload);
 
     // The log text area.
+    VerticalLayout rightColumnLayout = new VerticalLayout();
+    rightColumnLayout.setSpacing(true);
+    contentLayout.addComponent(rightColumnLayout);
+
     Label lbl = new Label("<h2>Upload Log</h2>", ContentMode.HTML);
-    contentLayout.addComponent(lbl);
+    rightColumnLayout.addComponent(lbl);
 
     logTxt = new TextArea();
-    logTxt.setColumns(80);
-    logTxt.setRows(15);
-    contentLayout.addComponent(logTxt);
+    logTxt.setRows(35);
+    logTxt.setWidth(FULL_WIDTH);
+    rightColumnLayout.addComponent(logTxt);
   }
 
   private void addExample(String title, Plupload upload) {
@@ -81,14 +96,14 @@ public class PluploadDemo extends UI {
     upload.addProgressListener(new BarProgressListener(progressBar));
 
     Label lbl = new Label("<h2>" + title + "</h2>", ContentMode.HTML);
-    contentLayout.addComponent(lbl);
+    leftColumnLayout.addComponent(lbl);
 
     HorizontalLayout rowLayout = new HorizontalLayout();
     rowLayout.setSpacing(true);
     rowLayout.addComponent(upload);
     rowLayout.addComponent(progressBar);
 
-    contentLayout.addComponent(rowLayout);
+    leftColumnLayout.addComponent(rowLayout);
   }
 
   private void log(String msg, Object... args) {
@@ -106,11 +121,12 @@ public class PluploadDemo extends UI {
   private Plupload buildPlupload() {
 
     final Plupload upload = new Plupload();
-    upload.setChunkSize(512l * 1024l);
+    upload.setChunkSize(512 * 1024);
+    upload.setMaxRetryBufferSize(upload.getChunkSize() * 2);
     upload.setMaxFileSize(500 * 1024 * 1024);
     upload.setMaxRetries(2);
     upload.setButtonCaption("Upload File");
-    upload.setReceiver(new NullReceiver());
+    upload.setReceiver(new DemoReceiver(new CountingDigestOutputStream()));
 
     upload.addStartedListener(new Plupload.StartedListener() {
       @Override
@@ -132,10 +148,21 @@ public class PluploadDemo extends UI {
     upload.addFinishedListener(new Plupload.FinishedListener() {
       @Override
       public void uploadFinished(Plupload.FinishedEvent evt) {
-        log("Upload of file %s finished with size %d.", evt.getFilename(),
-            evt.getLength());
-
         upload.setEnabled(true);
+
+        String hash = null;
+        int count = 0;
+        if (upload.getReceiver() instanceof DemoReceiver) {
+          CountingDigestOutputStream o = ((DemoReceiver) upload.getReceiver()).
+              getOutputStream();
+          hash = o.getHash();
+          count = o.getCount();
+          o.reset();
+        }
+
+        log("Upload of file %s finished with reported size %d, "
+            + "actual size %d, and MD5 hash %s.", evt.getFilename(),
+            evt.getLength(), count, hash);
       }
     });
 
@@ -164,51 +191,88 @@ public class PluploadDemo extends UI {
     }
   }
 
-  private class NullReceiver implements Upload.Receiver {
+  private class CountingDigestOutputStream extends OutputStream {
+
+    private int count = 0;
+    private MessageDigest md;
+
+    public CountingDigestOutputStream() {
+      reset();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+      count++;
+      md.update((byte) b);
+    }
+
+    public void reset() {
+      count = 0;
+
+      try {
+        md = MessageDigest.getInstance("MD5");
+      }
+      catch (NoSuchAlgorithmException ex) {
+        throw new RuntimeException("Unable to create MD5 digest.", ex);
+      }
+    }
+
+    public int getCount() {
+      return count;
+    }
+
+    public String getHash() {
+      byte[] hash = md.digest();
+
+      BigInteger bigInt = new BigInteger(1, hash);
+      String hashText = bigInt.toString(16);
+      while (hashText.length() < 32) {
+        hashText = "0" + hashText;
+      }
+
+      return hashText;
+    }
+  }
+
+  private class SlowOutputStream extends CountingDigestOutputStream {
+
+    @Override
+    public void write(byte[] b) throws IOException {
+
+      if (getCount() % 1024 == 0) {
+        try {
+          Thread.sleep(100);
+        }
+        catch (InterruptedException ex) {
+          // ignore
+        }
+      }
+
+      super.write(b);
+    }
+
+  }
+
+  private class DemoReceiver implements Upload.Receiver {
+
+    private final CountingDigestOutputStream outstream;
+
+    public DemoReceiver(CountingDigestOutputStream outstream) {
+      this.outstream = outstream;
+    }
 
     @Override
     public OutputStream receiveUpload(String filename, String mimeType) {
       log("Creating receiver output stream for file %s and mime-type %s.",
           filename, mimeType);
-      return new OutputStream() {
-        @Override
-        public void write(int b) throws IOException {
-        }
-      };
+
+      return outstream;
+    }
+
+    public CountingDigestOutputStream getOutputStream() {
+      return outstream;
     }
   }
 
-  private class SlowReceiver implements Upload.Receiver {
-
-    private final long sleepMillis;
-
-    public SlowReceiver(long sleepMillis) {
-      this.sleepMillis = sleepMillis;
-    }
-
-    @Override
-    public OutputStream receiveUpload(String filename, String mimeType) {
-      log("Creating receiver output stream for file %s and mime-type %s.",
-          filename, mimeType);
-      return new OutputStream() {
-        private long count = 0;
-
-        @Override
-        public void write(int b) throws IOException {
-          count++;
-
-          if (count % 1024 == 0) {
-            try {
-              Thread.sleep(sleepMillis);
-            }
-            catch (InterruptedException ex) {
-              // ignore
-            }
-          }
-        }
-      };
-    }
-
-  }
 
 }
